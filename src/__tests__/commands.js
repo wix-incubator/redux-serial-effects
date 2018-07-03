d4a586c3f2e25e757d745e377ac461930ae8ab21
Dor Itzhaki <dor-itzhaki@users.noreply.github.com>
2018-07-03 13:35:09 +0300

    Expose command helpers (#2)
    and some other small env issues

--- src/__tests__/commands.js
+++ src/__tests__/commands.js
@@ -1,33 +1,64 @@
 /* eslint-env jest */
 'use strict'
 
-const { createImmediateCmd, createQueuedCmd } = require('../commands')
+const {
+  createImmediateCmd,
+  createQueuedCmd,
+  isImmediateCommand,
+  isQueuedCommand
+} = require('../index')
 
-describe('base service', function() {
-  test('should return an immediate command object with the correct properties', function() {
-    expect(
-      createImmediateCmd(
-        'testExecutorType',
-        {
+describe('commands', function() {
+  describe('createImmediateCmd', function() {
+    test('should return an immediate command object with the correct properties', function() {
+      expect(
+        createImmediateCmd(
+          'testExecutorType',
+          {
+            type: 'testCommandType',
+            testProperty: 'someString'
+          },
+          'testMessageType'
+        )
+      ).toMatchObject({
+        type: 'testExecutorType',
+        isQueued: false,
+        actionType: 'testMessageType',
+        command: {
           type: 'testCommandType',
           testProperty: 'someString'
-        },
-        'testMessageType'
-      )
-    ).toMatchObject({
-      type: 'testExecutorType',
-      isQueued: false,
-      actionType: 'testMessageType',
-      command: {
-        type: 'testCommandType',
-        testProperty: 'someString'
-      }
+        }
+      })
     })
   })
 
-  test('should return a queued command object with the correct properties', function() {
-    expect(
-      createQueuedCmd(
+  describe('createQueuedCmd', function() {
+    test('should return a queued command object with the correct properties', function() {
+      expect(
+        createQueuedCmd(
+          'testExecutorType',
+          {
+            type: 'testCommandType',
+            testProperty: 'someString'
+          },
+          'testMessageType'
+        )
+      ).toMatchObject({
+        type: 'testExecutorType',
+        isQueued: true,
+        actionType: 'testMessageType',
+        command: {
+          type: 'testCommandType',
+
+          testProperty: 'someString'
+        }
+      })
+    })
+  })
+
+  describe('isImmediateCommand', function() {
+    test('should return true for an immediate command', function() {
+      const immediateCommand = createImmediateCmd(
         'testExecutorType',
         {
           type: 'testCommandType',
@@ -35,15 +66,79 @@ describe('base service', function() {
         },
         'testMessageType'
       )
-    ).toMatchObject({
-      type: 'testExecutorType',
-      isQueued: true,
-      actionType: 'testMessageType',
-      command: {
-        type: 'testCommandType',
+      expect(isImmediateCommand(immediateCommand)).toBe(true)
+    })
 
-        testProperty: 'someString'
+    test('should return false for a queued command', function() {
+      const queuedCommand = createQueuedCmd(
+        'testExecutorType',
+        {
+          type: 'testCommandType',
+          testProperty: 'someString'
+        },
+        'testMessageType'
+      )
+      expect(isImmediateCommand(queuedCommand)).toBe(false)
+    })
+
+    test('should return false for non commands', function() {
+      const nothing = null
+      const commandWithoutType = {
+        isQueued: false,
+        command: { type: 'test' },
+        actionType: 'testMessageType'
       }
+      const commandWithoutIsQueued = {
+        type: 'testExecutorType',
+        command: { type: 'test' },
+        actionType: 'testMessageType'
+      }
+      expect(isImmediateCommand(nothing)).toBe(false)
+      expect(isImmediateCommand(commandWithoutType)).toBe(false)
+      expect(isImmediateCommand(commandWithoutIsQueued)).toBe(false)
+    })
+  })
+
+  describe('isQueuedCommand', function() {
+    test('should return false for an immediate command', function() {
+      const immediateCommand = createImmediateCmd(
+        'testExecutorType',
+        {
+          type: 'testCommandType',
+          testProperty: 'someString'
+        },
+        'testMessageType'
+      )
+      expect(isQueuedCommand(immediateCommand)).toBe(false)
+    })
+
+    test('should return true for a queued command', function() {
+      const queuedCommand = createQueuedCmd(
+        'testExecutorType',
+        {
+          type: 'testCommandType',
+          testProperty: 'someString'
+        },
+        'testMessageType'
+      )
+      expect(isQueuedCommand(queuedCommand)).toBe(true)
+    })
+
+    test('should return false for non commands', function() {
+      const nothing = null
+      const commandWithoutType = {
+        isQueued: false,
+        command: { type: 'test' },
+        actionType: 'testMessageType'
+      }
+      const commandWithoutIsQueued = {
+        type: 'testExecutorType',
+        command: { type: 'test' },
+        actionType: 'testMessageType'
+      }
+      expect(isQueuedCommand(nothing)).toBe(false)
+      expect(isQueuedCommand(commandWithoutType)).toBe(false)
+      expect(isQueuedCommand(commandWithoutIsQueued)).toBe(false)
     })
   })
 })
--- src/index.js
+++ src/index.js
@@ -2,12 +2,18 @@
 
 const createMiddleware = require('./middleware')
 const combineSubscribers = require('./combineSubscribers')
-const { createImmediateCmd, createQueuedCmd } = require('./commands')
+const {
+  createImmediateCmd,
+  createQueuedCmd,
+  isImmediateCommand,
+  isQueuedCommand
+} = require('./commands')
 const { match: matchAction } = require('./action')
 
 module.exports.createMiddleware = createMiddleware
 module.exports.combineSubscribers = combineSubscribers
 module.exports.createImmediateCmd = createImmediateCmd
 module.exports.createQueuedCmd = createQueuedCmd
-
+module.exports.isImmediateCommand = isImmediateCommand
+module.exports.isQueuedCommand = isQueuedCommand
 module.exports.matchAction = matchAction
