fcea7a5d94262e0824d2cfb48f7371ce361a8224
Dor Itzhaki <dori@wix.com>
2018-10-14 09:57:43 +0300

    removed combineSubscribers

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 'use strict'
 
 const createMiddleware = require('./middleware')
-const combineSubscribers = require('./combineSubscribers')
 const { match: matchAction } = require('./action')
 
 module.exports = {
   createMiddleware,
-  combineSubscribers,
   matchAction
 }
--- src/utils/flatten.js
+++
@@ -1,5 +0,0 @@
-'use strict'
-
-const flatten = array => array.reduce((acc, val) => acc.concat(val), [])
-
-module.exports.flatten = flatten
